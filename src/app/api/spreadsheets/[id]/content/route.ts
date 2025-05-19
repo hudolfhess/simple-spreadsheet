596d42a2422333d0dbe57f10e05ac66f5fe2bdc4
hudolfhess <hudolf@gmail.com>
2025-05-19 19:14:13 -0300

    removed comments from spreadsheets/:id/content/route

--- src/app/api/spreadsheets/[id]/content/route.ts
+++ src/app/api/spreadsheets/[id]/content/route.ts
@@ -19,31 +19,6 @@ export async function GET(
   });
 
   return NextResponse.json(JSON.parse(spreadsheetContent?.content));
-
-  // return NextResponse.json({
-  //   0: {
-  //     0: { value: "Salários" },
-  //     1: { value: "5300.00", type: "decimal", referencedBy: [[2, 1]] },
-  //     2: { value: id, type: "string" },
-  //     3: { value: spreadsheet.name, type: "string" },
-  //   },
-  //   1: {
-  //     0: { value: "Aluguel" },
-  //     1: { value: "800.00", type: "decimal" },
-  //   },
-  //   2: {
-  //     0: { value: "Total" },
-  //     1: {
-  //       value: "4500.00",
-  //       type: "decimal",
-  //       formula: "=B1-B2",
-  //       referencesTo: [
-  //         [0, 1],
-  //         [1, 1],
-  //       ],
-  //     },
-  //   },
-  // });
 }
 
 export async function PUT(
