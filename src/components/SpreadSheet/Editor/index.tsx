7211f8143fe241b41f6df0ed2762e56e633ed1e9
hudolfhess <hudolf@gmail.com>
2025-05-21 08:44:00 -0300

    component Editor that allows to edit spreadsheet's name

---
+++ src/components/SpreadSheet/Editor/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import {
+  getSpreadSheetById,
+  updateSpreadSheetById,
+} from "@/http_clients/spreadsheets";
+import Link from "next/link";
+import { SpreadSheetEntity } from "@/commons/entities/SpreadSheetEntity";
+
+export default function Editor(props: { id: string }) {
+  const [data, setData] = useState<SpreadSheetEntity>({
+    id: props.id,
+    name: "",
+    updatedAt: "",
+    createdAt: "",
+  });
+  const [editionMode, setEditionMode] = useState(false);
+  const [editValue, setEditValue] = useState("");
+
+  useEffect(() => {
+    getSpreadSheetById(props.id).then(setData);
+  }, [props.id]);
+
+  useEffect(() => {
+    setEditValue(data.name);
+  }, [data]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      updateSpreadSheetById(props.id, e.currentTarget.value).then(setData);
+      setEditionMode(false);
+    } else if (e.key === "Escape") {
+      setEditValue(data.name);
+      setEditionMode(false);
+    }
+  };
+
+  return (
+    <div className="spreadsheet-header">
+      <Link href="/spreadsheets/">Voltar</Link>
+      <h1>Spreadsheet</h1>
+      {editionMode ? (
+        <input
+          type="text"
+          value={editValue}
+          onBlur={() => {
+            setEditValue(data.name);
+            setEditionMode(false);
+          }}
+          onKeyDown={handleKeyDown}
+          onChange={(e) => setEditValue(e.target.value)}
+        />
+      ) : (
+        <button onClick={() => setEditionMode(true)}>{data.name}</button>
+      )}
+    </div>
+  );
+}
